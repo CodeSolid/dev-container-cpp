798ec584961a642844e47db7adcfedcd0951b70c
John Lockwood <john@codesolid.com>
2023-08-10 19:31:18 +0000

    Documentation hello.cpp

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,5 +1,9 @@
+// compile and run with:
+// g++ hello.cpp -o hello && ./hello
+
 #include <iostream>
 
 int main(int argc, char* argv[]) {
     std::cout << "Hello, world!" << std::endl;
+    return 0;
 }
